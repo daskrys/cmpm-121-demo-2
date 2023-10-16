95d0e4e93f945e44b9f823095990b21dfab4dfc5
daskrys <caperezl@ucsc.edu>
2023-10-16 11:01:05 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Demo 2";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+let canvas = document.querySelector<HTMLDivElement>("#canvas")!;
+canvas.innerHTML = "<canvas id='myCanvas'></canvas>";
