c6faaffcec27130d250afa8a2a69575a9f4f2aa5
daskrys <caperezl@ucsc.edu>
2023-10-16 15:06:39 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import "./style.css";
+import "./draw.ts";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
@@ -10,5 +11,41 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-let canvas = document.querySelector<HTMLDivElement>("#canvas")!;
-canvas.innerHTML = "<canvas id='myCanvas'></canvas>";
+("use strict");
+
+const canvas = document.createElement("canvas");
+canvas.id = "myCanvas";
+app.append(canvas);
+
+const ctx = canvas.getContext("2d")!;
+const cursor = { active: false, x: 0, y: 0 };
+const paths = [];
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
